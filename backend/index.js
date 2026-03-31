53833b61a08717d8aabd5c101a3d39aaa7dda331
Nikhil Saini <nikhilsainiofficial546@gmail.com>
2026-03-31 12:59:50 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -151,7 +151,7 @@ async function run(){
 
   console.log(`DB connected`);
 
-    server.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT}`);
   });
 }
