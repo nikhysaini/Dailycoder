b56ec7b1a9002a769524310f985a22439cedca59
nikhysaini <nikhilsainiofficial546@gmail.com>
2026-05-09 10:15:50 +0530

    1

--- frontend/src/page/home.jsx
+++ frontend/src/page/home.jsx
@@ -2,16 +2,17 @@ import { useState, useEffect } from "react";
 import { TypeAnimation } from "react-type-animation";
 import {ReactTypingEffect} from "react-typing-effect";
 import { motion } from "framer-motion";
+import { Outlet , useNavigate,useLocation , Link} from "react-router-dom";
 
 export default function Home() {
-
+const navigate = useNavigate();
 return (<div className="min-h-screen bg-gray-100 py-16 pt-20 px-16">
 
  <div className="bg-gray-200/10 hover:bg-white/15 shadow-xl rounded transform transition grid lg:grid-cols-2 sm:grid-cols-1 gap-2 gap-y-7 text-center p-10 py-26">
    <div className="">
      <div className="text-5xl  font-serif text-red-600/50">One <span className="text-blue-600">Problem</span> a day</div>
      <div className="text-md text-center pl-5 pt-1 ml-5 text-black">@ keeps rejection away</div>
-     <div className="text-center"><button className="w-50 bg-green-400 hover:bg-green-500/90 rounded-lg text-lg cursor-pointer text-center mt-12 mr-50 px-2 py-2 text-black mx-auto">Solve Today problem</button></div>
+     <div className="text-center"><button onClick={ () => navigate("/problem") } className="w-58 bg-green-400 hover:bg-green-500/90 rounded-lg text-lg cursor-pointer text-center mt-12 mr-50 px-2 py-2 text-black mx-auto">Solve premium problems</button></div>
     </div>
    
 
